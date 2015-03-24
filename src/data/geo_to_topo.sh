e123ace9198eefda6a7a6e53116f5eb4e5954951
Andrew Chavez <adchavez@gmail.com>
2015-03-24 13:52:54 -0500

    Merge in population data with topojson CLI

--- src/data/geo_to_topo.sh
+++ src/data/geo_to_topo.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 topojson -o ../../public/data/counties.topojson \
   --quantization 1e3 \
+  --external-properties CO-EST2014-alldata.csv \
   --simplify-proportion 0.25 \
-  --id-property NAME \
+  --properties p2010=+POPESTIMATE2010,p2011=+POPESTIMATE2011,p2012=+POPESTIMATE2012,p2013=+POPESTIMATE2013,p2014=+POPESTIMATE2014 \
+  --id-property NAMELSAD \
   tl_2014_us_county_texas.geojson
