ae25e0097b1026c172d0fef0f9390628205cc772
Andrew Chavez <adchavez@gmail.com>
2015-03-24 16:57:59 -0500

    Year controls; Add fixed color scale for now

--- src/js/app.js
+++ src/js/app.js
@@ -2,84 +2,80 @@ require(['d3', 'topojson', 'd3-tip'], function(d3, topojson, d3tip) {
 
   'use strict';
 
+  var startYear = d3.select('#start-year'),
+      endYear = d3.select('#end-year');
+
+  // The (default) years to compare
+  var compare = {
+    start: startYear[0][0].value,
+    end: endYear[0][0].value
+  };
+
+  // Setup our SVG
+  var width = 960, height = 900;
+
+  var svg = d3.select('#county-map').append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+  // Setup the projection
+  var projection = d3.geo.conicConformal()
+    .center([2, 31.15])
+    .rotate([102, 0])
+    .scale(5550)
+    .translate([width / 2, height / 2]);
+
+  // Path generator
+  var path = d3.geo.path()
+    .projection(projection);
+
+  // Calculate percent change
+  var percentChange = function(start, end) {
+    return (end - start) / start;
+  };
+
+  // A formatter for the whole population numbers
+  var popFormat = d3.format('0,0');
+  var changeFormat = function(d) {
+    if(d < 0) {
+      d *= -1;
+    }
+    return d3.round(d * 100, 1) + '%';
+  };
+
+  // Helper that generates the population change snippet in the tooltip
+  var popChangeText = function(start, end) {
+    var text = ' <span class="pull-right">';
+    if(end > start) {
+      text += '<i class="fa fa-arrow-up"></i>';
+    }
+    else if (end < start) {
+      text += '<i class="fa fa-arrow-down"></i>';
+    }
+    return text + ' ' + changeFormat(percentChange(start, end)) + '</span>';
+  };
+
+  // Color scale
+  var color = d3.scale.linear()
+    .clamp(true)
+    .domain([-0.1, 0, 0.1])
+    .range(['darkred', 'white', 'darkgreen']);
+
+  // A helper to shade the counties baed on percent change
+  var shadeCounties = function() {
+    this.attr('fill', function(d) {
+      return color(percentChange(d.properties['p' + compare.start], d.properties['p' + compare.end]));
+    });
+  };
+
   d3.json('data/counties.topojson', function(err, counties) {
     if (err) return console.error(err);
-
-    var width = 960,
-    height = 900;
-
-    var svg = d3.select('#county-map').append('svg')
-        .attr('width', width)
-        .attr('height', height);
 
     // Get the county geos from the topo file
     var countyGeos = topojson.feature(counties, counties.objects.tl_2014_us_county_texas);
 
-    // Setup the projection
-    var projection = d3.geo.conicConformal()
-      .center([2, 31.15])
-      .rotate([102, 0])
-      .scale(5550)
-      .translate([width / 2, height / 2]);
-
-    // Path generator
-    var path = d3.geo.path()
-      .projection(projection);
-
-    // The years to compare
-    var compare = {
-      start: 2010,
-      end: 2014
-    };
-
-    // Calculate percent change
-    var percentChange = function(start, end) {
-      return (end - start) / start;
-    };
-
     // Extract features from topojson
     var countyGeo = topojson.feature(counties, counties.objects.tl_2014_us_county_texas).features;
-
-    // Add percent change, so we can set our color scale
-    var data = countyGeo.map(function(d) {
-      d.change = percentChange(d.properties['p' + compare.start], d.properties['p' + compare.end]);
-      return d;
-    });
-
-    // Color scale
-    var color = d3.scale.linear()
-      .domain([
-        d3.min(data, function(d) {
-          return d.change;
-        }),
-        0,
-        d3.max(data, function(d) {
-          return d.change;
-        })
-      ])
-      .range(['darkred', 'white', 'darkgreen']);
-      //.range(['rgb(131, 29, 29)', '#fff', 'rgb(8, 48, 107)']);
-
-    // A formatter for the whole population numbers
-    var popFormat = d3.format('0,0');
-    var changeFormat = function(d) {
-      if(d < 0) {
-        d *= -1;
-      }
-      return d3.round(d * 100, 1) + '%';
-    };
-
-    // Helper that generates the population change snippet in the tooltip
-    var popChangeText = function(start, end) {
-      var text = ' <span class="pull-right">';
-      if(end > start) {
-        text += '<i class="fa fa-arrow-up"></i>';
-      }
-      else if (end < start) {
-        text += '<i class="fa fa-arrow-down"></i>';
-      }
-      return text + ' ' + changeFormat(percentChange(start, end)) + '</span>';
-    };
 
     // Enable tool tips
     var tip = d3tip()
@@ -101,15 +97,16 @@ require(['d3', 'topojson', 'd3-tip'], function(d3, topojson, d3tip) {
     svg.call(tip);
 
     // Add paths for counties
-    svg.selectAll('.county')
-      .data(data)
-    .enter().append('path')
-      .attr('fill', function(d) {
-        return color(d.change);
-      })
-      .attr('d', path)
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide);
+    svg.append('g')
+      .attr('class', 'counties')
+      .selectAll('.county')
+      .data(countyGeo)
+      .enter()
+        .append('path')
+        .call(shadeCounties)
+        .attr('d', path)
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide);
 
     // Add inner boundaries
     svg.append('path')
@@ -126,6 +123,21 @@ require(['d3', 'topojson', 'd3-tip'], function(d3, topojson, d3tip) {
       }))
       .attr('d', path)
       .attr('class', 'county-borders');
+
+    // Handle year chages
+    var yearChange = function() {
+      svg.select('.counties')
+        .selectAll('path')
+        .call(shadeCounties);
+    };
+    startYear.on('input', function() {
+      compare.start = this.value;
+      yearChange();
+    });
+    endYear.on('input', function() {
+      compare.end = this.value;
+      yearChange();
+    });
   });
 
 });
